79a7b79de140e2478e01135c9beef7ce3062009f
Charles Oroko <charles.oroko@outlook.com>
2025-09-28 05:07:42 -0500

    chore: add icons to nav itemsw

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,15 +5,74 @@
 	let { children } = $props();
 </script>
 
+{#snippet navitems()}
+	<li>
+		<a href="/">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="size-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+				/>
+			</svg>
+			<span>About Me</span>
+		</a>
+	</li>
+	<li>
+		<a href="/projects">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="size-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+				/>
+			</svg>
+			<span>Projects</span>
+		</a>
+	</li>
+	<li>
+		<a href="/blog">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="size-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z"
+				/>
+			</svg>
+			<span>Blog</span>
+		</a>
+	</li>
+{/snippet}
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="font-nunito drawer">
+<div class="font-nunito drawer bg-base-100">
 	<input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
 	<div class="drawer-content flex flex-col">
 		<!-- Navbar -->
-		<header class="navbar w-full">
+		<header class="navbar w-full border-b border-base-300">
 			<div class="flex-none lg:hidden">
 				<label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
 					<svg
@@ -34,24 +93,18 @@
 			<div class="mx-2 flex-1 px-2">Ogamba Creates</div>
 			<nav class="hidden flex-none lg:block">
 				<ul class="menu menu-horizontal">
-					<!-- Navbar menu content here -->
-					<li><a href="/">Ogamba Creates</a></li>
-					<li><a href="/projects">Projects</a></li>
-					<li><a href="/blog">Blog</a></li>
+					{@render navitems()}
 				</ul>
 			</nav>
 		</header>
-		<main class="py-4 md:mx-auto">
+		<main class="w-full p-4 md:mx-auto md:w-xl md:px-0">
 			{@render children?.()}
 		</main>
 	</div>
 	<div class="drawer-side">
 		<label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
 		<ul class="menu min-h-full w-80 p-4">
-			<!-- Sidebar content here -->
-			<li><a href="/">Ogamba Creates</a></li>
-			<li><a href="/projects">Projects</a></li>
-			<li><a href="/blog">Blog</a></li>
+			{@render navitems()}
 		</ul>
 	</div>
 </div>
